7da045fcbbc73d29723f940e70d257db4be22b30
jrwooda3 <jrwooda3@ncsu.edu>
2023-07-31 21:32:24 -0400

    Added apex map rotation command. Still waiting on tracker.gg approval

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
-import http.client
 import random
+import requests
 
 import discord
 from dotenv import load_dotenv
@@ -9,7 +9,10 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-APEX_KEY = os.getenv('APEX_KEY')
+
+## Worse Apex API, being used for map pool while waiting for perms
+## https://apexlegendsstatus.com/
+APEX_KEY = os.getenv('APEXSTATUS_KEY')
 
 ## Permissions of the bot, diff from actual bot permissions
 intents = discord.Intents.default()
@@ -24,33 +27,62 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print("Sanctuary Bot is ready...")
 
+### What the bot does when a message is sent.... it's always watching ###
 @client.event
 async def on_message(message):
+    # Bot can't respond to itself #
     if message.author == client.user:
         return
     else:
         # Simple reply
         if message.content.lower() == 'hello':
             await message.reply(f"Hello, {message.author}")
-        # if the message starts with '%'
+            
+            
+        ### Start of command readings ###
+        ### Can have multiple command prompts i.e. not just '%' ###
+        ### But may interfere with existing bots in server ###
         elif message.content.lower()[0] == '%':
             command = message.content.split('%')[1]
             command_args = command.split(' ')
-            # gaypex command
-            if (command_args[0].lower() == 'gaypex'):
-                await gaypex(command_args, message.channel)
-            #coinflip command
+            ## Prompt for all apex commands ##
+            ## '%apex [arg1] [arg2]...' ##
+            if (command_args[0].lower() == 'apex'):
+                await apex(command_args, message)
+            ## Prompt for coinflip ##
+            ## '%coinflip' ##
             if (command_args[0].lower() == 'coinflip'):
                 await coinflip(message)
             
-# Request for apex tracker. API still needs approval
-async def gaypex(command, message):
-    await message.reply(f'{command}')
-    conn = http.client.HTTPSConnection('https://public-api.tracker.gg/apex/v1/standard/profile/5/', 443)
-    conn.request('GET', '/', headers={'TRN-Api-Key':APEX_KEY,
-                                      'Accept': 'application/vnd.api+json'})
-    r = conn.getresponse()
-    print(r.read())
+### Request for apex tracker. API still needs approval, so using worse api ###
+### '%apex [arg1] [arg2] [arg3]... ' ###
+### '%apex map' gives the current and next map ###
+async def apex(command, message):
+    auth = f'auth={APEX_KEY}'
+    base = f'https://api.mozambiquehe.re/'
+    
+    ## Filter the command. Since only 1 valid commant atm (map), all others are considered invalid ##
+    if command[1].lower() != 'map':
+        await message.reply("Invalid command. Expected: '\%apex map'")
+        return
+    
+    # Build headers for requests.get()
+    headers = {
+        'Authorization': APEX_KEY
+    # Save the response
+    }
+    response = requests.get(
+        url= (base + 'maprotation?' + auth),
+        headers=headers
+    )
+    # Make the response json
+    resp_json = response.json()
+    # Format the response
+    current_map = {'map':resp_json['current']['map'], 'time':resp_json['current']['remainingTimer']}
+    next_map = {'map':resp_json['next']['map'], 'time':resp_json['next']['DurationInMinutes']}
+    # Bot responds
+    await message.reply(f"{current_map['map']} has {current_map['time']} remaining.\nThe next map will be {next_map['map']} for {next_map['time']} minutes.\n")
+    
     
 # Coinflip using random library
 async def coinflip(message):
