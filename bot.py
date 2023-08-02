c1fc3f44be220abc50dc820e4678fc93052799fa
Nick T <terrelln19@students.ecu.edu>
2023-08-02 00:29:22 -0400

    Added functionality for all states and regions for golf moose. Pair program Nick and Jakob

--- bot.py
+++ bot.py
@@ -123,10 +123,94 @@ async def coinflip(message):
 # Checks current golf moose deals based on specified region
 ### Practice for web scraping/parsing raw html output ###
 
+### Checks golf moose for deals in the specified state/region ###
+### command_args in form 'golfmoose, [state], [region]' ###
+async def golfmoose(message, command_args):
+    working_msg = await message.reply("Working...")
+    
+    states = ['Arizona', 'Northern-California', 'Southern-California', 'Carolinas', 'Florida', 'Georgia', 'Illinois', 'Nevada', 'Oregon', 'Texas', 'Virginia', 'Washington', 'Wisconsin', 'Worldwide']
+    regions = {'arizona': ['Phoenix', 'Prescott', 'Sedona', 'Tucson'],
+               'northern-california': ['Central-Coast', 'East-Bay', 'Fresno', 'Lake-Tahoe', 'Monterey', 'North-Bay', 'North-State', 'Sacramento', 'San-Jose', 'Santa-Cruz', 'Sierra-Foothills', 'Stockton'],
+               'southern-California': ['Coast', 'Inland-Empire', 'Los-Angeles', 'Mojave', 'Palm-Springs', 'San-Diego', 'Santa-Barbara', 'Temecula'],
+               'carolinas':['Augusta', 'Coastal', 'Fayetteville', 'Greenville', 'Greenville-South-Carolina', 'Greensboro', 'Hilton-Head', 'Mountains', 'Myrtle-Beach', 'Pinehurst', 'Raleigh'],
+               'florida': ['Central', 'Central-East', 'Lakeland', 'Northeast-Florida', 'Orlando', 'Southeast', 'Southwest-Florida', 'Tampa'],
+               'georgia': ['Atlanta', 'Augusta-Georgia', 'Savannah'],
+               'illinois': ['Chicago-Northwest', 'Chicago-South', 'Chicago-Southwest', 'Chicago-West', 'Rockford', 'Southern-Illinois'],
+               'nevada': ['Las-Vegas', 'Reno', 'Mojave'],
+               'oregon': ['Bend', 'Eugene', 'Northeasten-Oregon', 'Portland', 'Southern-Oregon'],
+               'texas': ['Austin', 'Dallas-Fort-Worth', 'Hill-Country', 'San-Antonio'],
+               'virginia':['Hampton-Roads'],
+               'wisconsin': ['Central-Wisconsin', 'North', 'South', 'West'],
+               'worldwide':['Mexico']}
+    
+    if len(command_args) == 2 and command_args[1].lower() == 'states':
+        # list all available states
+        response = 'Supported States: \n'
+        for state in states:
+            formatted_list = f"{state} \n"
+            response += formatted_list
+        await message.reply(response)
+        await working_msg.delete()
+        return
+    
+    elif len(command_args) == 2 and command_args[1].lower() == 'washington':
+        await webscrape(message, command_args[1], None)
+        await working_msg.delete()
 
-async def golfmoose(message, command):
-    await message.reply("Working...")
-    region = command[1].lower()
+    elif len(command_args) == 2 and (command_args[1].lower() != 'states' and command_args.lower() != 'washington'):
+        # 1 command after 'golfmoose', 
+        await message.reply("Invalid command syntax")
+        
+    elif len(command_args) == 3 and command_args[1].lower() in (state.lower() for state in states) and command_args[2].lower() == 'regions':
+        # list all available regions in the specified state
+        response = f"Supported Regions for {command_args[1].upper()}: \n"
+        for region in regions[command_args[1].lower()]:
+            formatted_list = f"{region} \n"
+            response += formatted_list
+        await message.reply(response)
+        await working_msg.delete()
+        return
+
+    elif len(command_args) == 3 and command_args[1].lower() in (state.lower() for state in states) and command_args[2].lower() in (region.lower() for region in regions[command_args[1]]):
+        # run the meat and potatoes
+        await webscrape(message, command_args[1], command_args[2])
+        await working_msg.delete()
+        
+    else:
+        await message.reply("Invalid command syntax. Expected '%golfmoose [state] [region]'")
+        return
+
+    
+async def webscrape(message, state, region):
+    
+    def get_panel():
+        response = ''
+        deals = driver.find_elements(
+            By.CSS_SELECTOR, "div[class*='deal-single panel']")
+        deals_dict = {}
+        for deal in deals:
+            # find deal/prices
+            title = deal.find_element(
+                By.CLASS_NAME, "deal-title").text  # title
+            # desc = deal.find_element(By.CLASS_NAME, "text-muted").text # Deal
+            prices = deal.find_elements(
+                By.CSS_SELECTOR, "span[class*='woocommerce-Price-amount amount']")
+            prev_price = prices[0].find_element(By.TAG_NAME, "bdi").text
+            new_price = prices[1].find_element(By.TAG_NAME, "bdi").text
+            deal_dict = {
+                'title': title,
+                # 'desc': desc,
+                'prev_price': prev_price,
+                'new_price': new_price
+            }
+            deals_dict[title] = deal_dict
+
+        for key in deals_dict.keys():
+            formatted_deal = f"{deals_dict[key]['title']}   ({deals_dict[key]['new_price']}) \n"
+            response += formatted_deal
+
+        return response
+    
     # Selenium/Pandas Chrome Web Driver initialization
     # start by defining the options
     options = webdriver.ChromeOptions()
@@ -140,43 +224,33 @@ async def golfmoose(message, command):
     # pass the defined options and service objects to initialize the web driver
     driver = Chrome(options=options, service=chrome_service)
     driver.implicitly_wait(5)
+    
 
-    url = ('https://golfmoose.com/product-category/north-carolina/' + region)
+    ##CaliforniaS - central coast = coast - mojave = mojave-1
+    ##Carolinas - greenville(SC) = greenville-south-carolina
+    ##Florida - Northeast = northeast-florida - Southwest = southwest-florida
+    ##Georgia - Augusta = augusta-georgia
+    ##Texas - Austin = austintx
+    ##Wisconsin - Central = central-wisconsin
+    
+    # Washington special case
+    if state.lower() == 'washington':
+        url = ('https://golfmoose.com/product-category/' + state)
+    # Carolinas special case
+    elif state.lower() == 'carolinas':
+        url = ('https://golfmoose.com/product-category/north-carolina/' + region)
+    # Base case
+    else:
+        url = ('https://golfmoose.com/product-category/' + state + '/' + region)
     
     driver.get(url)
     time.sleep(4)
-
-    def get_panel():
-        response = ''
-        deals = driver.find_elements(By.CSS_SELECTOR, "div[class*='deal-single panel']")
-        deals_dict = {}
-        for deal in deals:
-            # find deal/prices
-            title = deal.find_element(By.CLASS_NAME, "deal-title").text # title
-            #desc = deal.find_element(By.CLASS_NAME, "text-muted").text # Deal
-            prices = deal.find_elements(By.CSS_SELECTOR, "span[class*='woocommerce-Price-amount amount']")
-            prev_price = prices[0].find_element(By.TAG_NAME, "bdi").text
-            new_price = prices[1].find_element(By.TAG_NAME, "bdi").text
-            deal_dict = {
-                'title': title,
-                #'desc': desc,
-                'prev_price': prev_price,
-                'new_price': new_price
-            }
-            deals_dict[title] = deal_dict
-            
-        for key in deals_dict.keys():
-            formatted_deal = f"{deals_dict[key]['title']}   ({deals_dict[key]['new_price']}) \n"
-            response += formatted_deal
-            
-        return response
     
     bot_response = get_panel()
     print(bot_response)
     await message.reply(bot_response)
     
-
-
+    
 # Guessing random number game
 async def randomGuess(message):
     await message.reply(f"Guess a number between 0 and 100")
